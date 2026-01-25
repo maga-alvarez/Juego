fbb88e8f4a61f53ddd84dfc5f6b19b415179172e
Noelia Magali Alvarez <noemagaalva@gmail.com>
2026-01-25 12:53:06 -0300

    Agrego menu y case 2

--- js/main.js
+++ js/main.js
@@ -6,19 +6,32 @@ console.log("Bienvenido/a " + nombredelpersonaje + "!")
 }
 saludar(nombredelpersonaje)
 
-const actividades = [comer, dormir, hidratarse, entrenar, correr, estirar]
+const actividades = ["dormir", "comer", "hidratarse", "entrar", "correr", "estirar"]
+const energias = [15, 10, 5, -15, -10, -5]
+const energiainicial = 25
 
-let menu = parseInt(prompt("Elije 1.ver energía actual \n 2. ver actividades \n 3. salir "))
+let menu = parseInt(prompt("Elije una opción \n 1.ver energía actual \n 2. ver actividades \n 3. salir "))
 
 while (menu !== 3) {
     switch (menu) {
         case 1: 
             enregiaactual ()
         case 2:
-            actividades()
+            for (let i = 0; i < actividades.length; i++){
+                    console.log(actividades[i], energias[i])
+                }
         default:
             alert ("opción incorrecta")
     }
     menu = parseInt(prompt("Elije 1.ver energía actual, 2. ver actividades, 3. salir "))
 }
+
+//Recorrer actividades
+
+for (let i = 0; i < actividades.length; i++){
+    console.log(actividades[i], energias[i])
+
+}
+
+
 
