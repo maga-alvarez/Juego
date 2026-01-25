02b6171fdbf34c4538157a809cac2e0c04c58d01
Noelia Magali Alvarez <noemagaalva@gmail.com>
2026-01-25 17:55:36 -0300

    Pruebo el menu de juego en el menu inicial

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const energias = [15, 10, 5, -15, -10, -5]
 function energiainicial (){
     console.log(" Tu energía inicial es 25")
 }
-
+let actividadeliminada = actividades.pop
 function eneregiactual(){
     let eneregia = energiainicial + actividad + energia;
 
@@ -31,29 +31,27 @@ while (menu !== 4) {
             for (let i = 0; i < actividades.length; i++){
                     console.log(actividades[i], energias[i]);
                 }
+                break
         case 3: 
-            juego()
-                
+            jugar()
+             let juego = parseInt(prompt("Elije una actividad: \n 1. Dormir \n 2. Comer \n 3. Hidratarse \n 4. Entrenar \n 5. Correr \n 6. salir"))
+             if (juego === 1) {console.log("Sumaste 10 de energia! Tu energía actual es: " + (energiainicial + 10));
+                    } else if (juego === 2) {
+                    console.log("Sumaste 15 de energía! Tu energía actual es: " + (energiainicial + 15));
+                    } else if (juego === 3) {
+                    console.log("Sumaste 5 de energía! Tu energía actual es: " + (energiainicial + 5));
+                    } else if (juego === 4) {
+                    console.log("Restaste 15 de energía! Tu energía actual es: " + (energiainicial - 15));
+                    } else if (juego === 5) {
+                    console.log("Restaste 10 de energía! Tu energía actual es: " + (energiainicial - 10));
+                    }
         default:
             alert ("opción incorrecta");
     }
     menu = parseInt(prompt("Elije una opción \n 1.ver energía inicial \n 2. ver actividades \n 3. elegir actividad \n 4. salir "))
 }
 
- let juego = parseInt(prompt("Elije una actividad: \n 1. Dormir \n 2. Comer \n 3. Hidratarse \n 4. Entrenar \n 5. Correr \n 6. salir"
-))
 
- if (juego === 1) {
-  console.log("Sumaste 10 de energia! Tu energía actual es: " + (energiainicial + 10));
-} else if (juego === 2) {
-  console.log("Sumaste 15 de energía! Tu energía actual es: " + (energiainicial + 15));
-} else if (juego === 3) {
-  console.log("Sumaste 5 de energía! Tu energía actual es: " + (energiainicial + 5));
-} else if (juego === 4) {
-  console.log("Restaste 15 de energía! Tu energía actual es: " + (energiainicial - 15));
-} else if (juego === 5) {
-  console.log("Restaste 10 de energía! Tu energía actual es: " + (energiainicial - 10));
-}
 
 
 
