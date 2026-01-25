3d82cf911910d25bf2b538ae7591661f53dac0bd
Noelia Magali Alvarez <noemagaalva@gmail.com>
2026-01-25 17:10:49 -0300

    Agrego energia inicial y acciones de menu

--- js/main.js
+++ js/main.js
@@ -8,20 +8,31 @@ saludar(nombredelpersonaje)
 
 const actividades = ["dormir", "comer", "hidratarse", "entrar", "correr", "estirar"]
 const energias = [15, 10, 5, -15, -10, -5]
-const energiainicial = 25
+function energiainicial (){
+    console.log(" Tu energía inicial es 25")
+}
 
-let menu = parseInt(prompt("Elije una opción \n 1.ver energía actual \n 2. ver actividades \n 3. salir "))
+function eneregiactual(){
+    let eneregia = energiainicial + actividad + energia;
+}
+
+
+
+let menu = parseInt(prompt("Elije una opción \n 1.ver energía inicial \n 2. ver actividades \n 3. salir "))
 
 while (menu !== 3) {
     switch (menu) {
         case 1: 
-            enregiaactual ()
+            energiainicial() 
+                console.log(energiainicial);
+                break
         case 2:
             for (let i = 0; i < actividades.length; i++){
-                    console.log(actividades[i], energias[i])
+                    console.log(actividades[i], energias[i]);
+                    break
                 }
         default:
-            alert ("opción incorrecta")
+            alert ("opción incorrecta");
     }
     menu = parseInt(prompt("Elije 1.ver energía actual, 2. ver actividades, 3. salir "))
 }
