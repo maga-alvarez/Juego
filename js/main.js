fd61a35b91dc895febfc0c9b46a7d55f5814a700
Noelia Magali Alvarez <noemagaalva@gmail.com>
2026-01-25 12:18:14 -0300

    Agrego contantes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 let nombredelpersonaje = prompt("Ingrese el nombre de su personaje")
 
+function saludar(nombredelpersonaje){
 alert("Bienvenido/a " + nombredelpersonaje + "!")
 console.log("Bienvenido/a " + nombredelpersonaje + "!")
+}
+saludar(nombredelpersonaje)
+
+const actividades = [comer, dormir, hidratarse, entrenar, correr, estirar]
+
+let menu = parseInt(prompt("Elije 1.ver energía actual \n 2. ver actividades \n 3. salir "))
+
+while (menu !== 3) {
+    switch (menu) {
+        case 1: 
+            enregiaactual ()
+        case 2:
+            actividades()
+        default:
+            alert ("opción incorrecta")
+    }
+    menu = parseInt(prompt("Elije 1.ver energía actual, 2. ver actividades, 3. salir "))
+}
 
