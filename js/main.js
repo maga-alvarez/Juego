d61583141ff5646f27bf47a06100e6f3cefdd503
Noelia Magali Alvarez <noemagaalva@gmail.com>
2026-02-07 13:36:36 -0300

    Agrego la funcion para recorrer el objeto

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 let nombredelpersonaje = prompt("Ingrese el nombre de su personaje")
 
 const actividades = {
-        "dormir": 15,
-        "comer": 10,
-        "hidratarse": 5,
-        "correr": -10,
-        "entrenar": -15
-}        
-        
-let energiainicial = 25
-
-function energia(energiainicial, actividades){
-        return energiainicial + actividades
+        dormir: 15,
+        comer: 10,
+        hidratarse: 5,
+        correr: -10,
+        entrenar: -15,
 }
+
+Object.entries(actividades).forEach(function (actividades) {
+        console.log(actividades)     
+});
+
+
+
 
