796215cea0cfa2b0efcece3c8fc598c0c2f2be55
Noelia Magali Alvarez <noemagaalva@gmail.com>
2026-02-10 17:46:23 -0300

    Agrego el primer array de objetos

--- js/main.js
+++ js/main.js
@@ -1,17 +1,49 @@
 let nombredelpersonaje = prompt("Ingrese el nombre de su personaje")
 
-const actividades = {
-        dormir: 15,
-        comer: 10,
-        hidratarse: 5,
-        correr: -10,
-        entrenar: -15,
-}
+const actividades = [
+        {
+                id: 1,
+                actividad: "Dormir",
+                energia: 15
+        },
+
+         {
+                id: 2,
+                actividad: "Comer",
+                energia: 10
+        },
+
+         {
+                id: 3,
+                actividad: "Hidratarse",
+                energia: 5
+        },
+        
+
+         {
+                id: 4,
+                actividad: "Entrenar",
+                energia: -15
+        },
+
+         {
+                id: 5,
+                actividad: "Correr",
+                energia: -10
+        }
+]
 
 Object.entries(actividades).forEach(function (actividades) {
         console.log(actividades)     
 });
 
 
+let energiaInicial = 25
 
+function energiaActual (){
+        function suma (energiaInicial){
+                
+
+        }
+}
 
