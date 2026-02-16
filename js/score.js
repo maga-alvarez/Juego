bfda1df39f0e1b3fbc972d59829c1c760b95a89b
Noelia Magali Alvarez <noemagaalva@gmail.com>
2026-02-16 15:21:02 -0300

    Arreglo lo del nombre

--- js/score.js
+++ js/score.js
@@ -3,9 +3,10 @@ const tabla = document.getElementById ("tablaPuntajes")
 const listaScore = JSON.parse(localStorage.getItem("ranking")) || []
 
 
+
 listaScore.forEach((puntuacion) => {
     const fila = document.createElement("tr")
-    fila.innerHTML = `<td>${puntuacion.agregarnombre}</td>
+    fila.innerHTML = `<td>${puntuacion.nombreUsuario}</td>
                     <td>${puntuacion.puntos}%</td>`
     tabla.appendChild(fila)
     
